8e33cbc5e314e84a6d3aac408901f0f1ea8bda10
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2025-03-14 23:03:48 +0300

    added permission request for taking photo

--- composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/views/AttachNotePhotoView.kt
+++ composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/views/AttachNotePhotoView.kt
@@ -14,6 +14,9 @@
 
 package com.ghostwalker18.schedule.views
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Environment
@@ -32,7 +35,9 @@ import androidx.compose.material.icons.filled.PhotoLibrary
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.FileProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ghostwalker18.schedule.ScheduleApp
@@ -45,6 +50,7 @@ import kotlin.random.Random
 
 @Composable
 actual fun AttachNotePhotoView(){
+    val context = LocalContext.current
     val model = viewModel { EditNoteModel() }
     var photoUri: String? = null
 
@@ -104,7 +110,17 @@ actual fun AttachNotePhotoView(){
             }
             IconButton(
                 onClick = {
-                    cameraPermissionLauncher.launch("")
+                    if (context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                        if (shouldShowRequestPermissionRationale(context as Activity, Manifest.permission.CAMERA)) {
+                            /*val toast = Toast.makeText(
+                                context,
+                                context.resources.getText(R.string.permission_for_photo), Toast.LENGTH_SHORT
+                            )
+                            toast.show()*/
+                        } else {
+                            cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
+                    }
                 },
                 modifier = Modifier
                     .padding(start = 10.dp)
@@ -129,3 +145,4 @@ private fun makeNotePhotoName(noteDate: Calendar): String {
     res += ".jpg"
     return res
 }
+
