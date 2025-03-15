a4d1dde99afeebbbd9fc4ea74a666f9a337ecdb8
ghostwalker18 <76010772+ghostwalker18@users.noreply.github.com>
2025-03-15 22:59:10 +0300

    completed widget settings

--- composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/ScheduleWidget.kt
+++ composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/ScheduleWidget.kt
@@ -32,90 +32,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class ScheduleWidget : AppWidgetProvider() {
-    private val timeFormat = SimpleDateFormat("HH:mm", Locale("ru"))
-    private val scope = CoroutineScope(Dispatchers.IO)
-
-    private fun updateAppWidget(
-        context: Context, appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-        val repository = getInstance().scheduleRepository
-        val appPreferences = getInstance().preferences
-        if (!checkNotificationsPermissions(context, appPreferences)
-            || !appPreferences.getBoolean("schedule_notifications", false)
-        ) {
-            repository.update()
-        }
-        // Construct the RemoteViews object
-        val views = RemoteViews(
-            context.packageName,
-            R.layout.schedule_widget_wrapper
-        )
-
-        val prefs = context.getSharedPreferences(
-            "WIDGET_$appWidgetId",
-            Context.MODE_PRIVATE
-        )
-
-        var group = prefs.getString("group", "last")
-        if (group == "last") {
-            group = repository.savedGroup ?: context.getString(R.string.not_mentioned)
-        }
-
-        val date = Calendar.getInstance()
-        when (prefs.getString("day", "")) {
-            "tomorrow" -> date.add(Calendar.DAY_OF_YEAR, 1)
-        }
-
-        val lessons = repository.getLessons(date, null, group)
-        scope.launch {
-            lessons.collect { lesson -> updateScheduleWidget(lesson, appWidgetId) }
-        }
-        val isEdited = prefs.getBoolean("isEdited", false)
-        if (isEdited) {
-            views.removeAllViews(R.id.widget_wrapper)
-
-            val isDynamicColorsEnabled = prefs.getBoolean("dynamic_colors", false)
-            val theme = prefs.getString("theme", "system")!!
-            val widgetLayoutId = getRequiredLayout(theme, isDynamicColorsEnabled)
-            val scheduleView = RemoteViews(context.packageName, widgetLayoutId)
-
-            val day = prefs.getString("day", "today")!!
-            when (day) {
-                "today" -> scheduleView.setTextViewText(R.id.day, context.getString(R.string.today))
-                "tomorrow" -> scheduleView.setTextViewText(R.id.day, context.getString(R.string.tomorrow))
-            }
-
-            views.addView(R.id.widget_wrapper, scheduleView)
-        }
-
-        views.setTextViewText(R.id.group, context.getString(R.string.for_group) + " " + group)
-        views.setTextViewText(
-            R.id.updated, context.getString(R.string.updated) + " " + timeFormat.format(date.time)
-        )
-
-        //setting action for refresh button: refresh schedule
-        val intentRefresh = Intent(
-            AppWidgetManager.ACTION_APPWIDGET_UPDATE,
-            null, context.applicationContext, ScheduleWidget::class.java
-        )
-        intentRefresh.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
-        val pendingRefresh = PendingIntent.getBroadcast(
-            context, appWidgetId,
-            intentRefresh, PendingIntent.FLAG_IMMUTABLE
-        )
-        views.setOnClickPendingIntent(R.id.updateButton, pendingRefresh)
-
-        //setting action for schedule tap: open app
-        val intentOpenApp = Intent(context, MainActivity::class.java)
-        val pendingOpenApp = PendingIntent.getActivity(
-            context, appWidgetId, intentOpenApp, PendingIntent.FLAG_IMMUTABLE
-        )
-        views.setOnClickPendingIntent(R.id.schedule, pendingOpenApp)
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
@@ -130,65 +46,152 @@ class ScheduleWidget : AppWidgetProvider() {
         }
     }
 
-    /**
-     * Этот метод используется для получения id нужного лэйаута.
-     * @param theme сохраненная тема
-     * @param isDynamicColorsEnabled выбрано ли использование динамических цветов
-     * @return id лэйаута
-     */
-    private fun getRequiredLayout(theme: String, isDynamicColorsEnabled: Boolean): Int {
-        return when(theme){
-            "system" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_daynight
-            else R.layout.schedule_widget_daynight
-            "night" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_night
-            else R.layout.schedule_widget_night
-            "day" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_day
-            else R.layout.schedule_widget_day
-            else -> R.layout.schedule_widget_daynight
-        }
-    }
+    companion object{
+        private val timeFormat = SimpleDateFormat("HH:mm", Locale("ru"))
+        private val scope = CoroutineScope(Dispatchers.IO)
 
-    /**
-     * Этот метод служит для обновления View виджета новыми занятиями.
-     *
-     * @author Ипатов Никита
-     * @since 5.0
-     */
-    private fun updateScheduleWidget(lessons: Array<Lesson>, id: Int) {
-        val context = getInstance().applicationContext
-        val views = RemoteViews(
-            context.packageName,
-            R.layout.schedule_widget_wrapper
-        )
-        views.removeAllViews(R.id.schedule)
-        if (lessons.isEmpty()) {
-            val placeholder = RemoteViews(
-                context.packageName,
-                R.layout.schedule_widget_row_placeholder
-            )
-            views.addView(R.id.schedule, placeholder)
-        }
-        var counter = 0
-        for (lesson in lessons) {
-            counter++
-            val lessonItem = RemoteViews(
-                context.packageName,
-                R.layout.schedule_widget_row_item
-            )
-            if (counter % 2 == 1) lessonItem.setInt(
-                R.id.row, "setBackgroundColor",
-                ContextCompat.getColor(context, R.color.gray_500)
-            )
-            with(lesson){
-                lessonItem.setTextViewText(R.id.lessonNumber, number)
-                lessonItem.setTextViewText(R.id.subjectName, subject)
-                lessonItem.setTextViewText(R.id.teacherName, teacher)
-                lessonItem.setTextViewText(R.id.roomNumber, room)
+        fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val repository = getInstance().scheduleRepository
+            val appPreferences = getInstance().preferences
+            if (!checkNotificationsPermissions(context, appPreferences)
+                || !appPreferences.getBoolean("schedule_notifications", false)
+            ) {
+                repository.update()
             }
-            views.addView(R.id.schedule, lessonItem)
+            // Construct the RemoteViews object
+            val views = RemoteViews(
+                context.packageName,
+                R.layout.schedule_widget_wrapper
+            )
+
+            val prefs = context.getSharedPreferences(
+                "WIDGET_$appWidgetId",
+                Context.MODE_PRIVATE
+            )
+
+            var group = prefs.getString("group", "last")
+            if (group == "last") {
+                group = repository.savedGroup ?: context.getString(R.string.not_mentioned)
+            }
+
+            val date = Calendar.getInstance()
+            when (prefs.getString("day", "")) {
+                "tomorrow" -> date.add(Calendar.DAY_OF_YEAR, 1)
+            }
+
+            val lessons = repository.getLessons(date, null, group)
+            scope.launch {
+                lessons.collect { lesson -> updateScheduleWidget(lesson, appWidgetId) }
+            }
+            val isEdited = prefs.getBoolean("isEdited", false)
+            if (isEdited) {
+                views.removeAllViews(R.id.widget_wrapper)
+
+                val isDynamicColorsEnabled = prefs.getBoolean("dynamic_colors", false)
+                val theme = prefs.getString("theme", "system")!!
+                val widgetLayoutId = getRequiredLayout(theme, isDynamicColorsEnabled)
+                val scheduleView = RemoteViews(context.packageName, widgetLayoutId)
+
+                val day = prefs.getString("day", "today")!!
+                when (day) {
+                    "today" -> scheduleView.setTextViewText(R.id.day, context.getString(R.string.today))
+                    "tomorrow" -> scheduleView.setTextViewText(R.id.day, context.getString(R.string.tomorrow))
+                }
+
+                views.addView(R.id.widget_wrapper, scheduleView)
+            }
+
+            views.setTextViewText(R.id.group, context.getString(R.string.for_group) + " " + group)
+            views.setTextViewText(
+                R.id.updated, context.getString(R.string.updated) + " " + timeFormat.format(date.time)
+            )
+
+            //setting action for refresh button: refresh schedule
+            val intentRefresh = Intent(
+                AppWidgetManager.ACTION_APPWIDGET_UPDATE,
+                null, context.applicationContext, ScheduleWidget::class.java
+            )
+            intentRefresh.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, intArrayOf(appWidgetId))
+            val pendingRefresh = PendingIntent.getBroadcast(
+                context, appWidgetId,
+                intentRefresh, PendingIntent.FLAG_IMMUTABLE
+            )
+            views.setOnClickPendingIntent(R.id.updateButton, pendingRefresh)
+
+            //setting action for schedule tap: open app
+            val intentOpenApp = Intent(context, MainActivity::class.java)
+            val pendingOpenApp = PendingIntent.getActivity(
+                context, appWidgetId, intentOpenApp, PendingIntent.FLAG_IMMUTABLE
+            )
+            views.setOnClickPendingIntent(R.id.schedule, pendingOpenApp)
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
 
-        val appWidgetManager = AppWidgetManager.getInstance(context)
-        appWidgetManager.partiallyUpdateAppWidget(id, views)
+        /**
+         * Этот метод служит для обновления View виджета новыми занятиями.
+         *
+         * @author Ипатов Никита
+         * @since 5.0
+         */
+        private fun updateScheduleWidget(lessons: Array<Lesson>, id: Int) {
+            val context = getInstance().applicationContext
+            val views = RemoteViews(
+                context.packageName,
+                R.layout.schedule_widget_wrapper
+            )
+            views.removeAllViews(R.id.schedule)
+            if (lessons.isEmpty()) {
+                val placeholder = RemoteViews(
+                    context.packageName,
+                    R.layout.schedule_widget_row_placeholder
+                )
+                views.addView(R.id.schedule, placeholder)
+            }
+            var counter = 0
+            for (lesson in lessons) {
+                counter++
+                val lessonItem = RemoteViews(
+                    context.packageName,
+                    R.layout.schedule_widget_row_item
+                )
+                if (counter % 2 == 1) lessonItem.setInt(
+                    R.id.row, "setBackgroundColor",
+                    ContextCompat.getColor(context, R.color.gray_500)
+                )
+                with(lesson){
+                    lessonItem.setTextViewText(R.id.lessonNumber, number)
+                    lessonItem.setTextViewText(R.id.subjectName, subject)
+                    lessonItem.setTextViewText(R.id.teacherName, teacher)
+                    lessonItem.setTextViewText(R.id.roomNumber, room)
+                }
+                views.addView(R.id.schedule, lessonItem)
+            }
+
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            appWidgetManager.partiallyUpdateAppWidget(id, views)
+        }
+
+        /**
+         * Этот метод используется для получения id нужного лэйаута.
+         * @param theme сохраненная тема
+         * @param isDynamicColorsEnabled выбрано ли использование динамических цветов
+         * @return id лэйаута
+         */
+        private fun getRequiredLayout(theme: String, isDynamicColorsEnabled: Boolean): Int {
+            return when(theme){
+                "system" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_daynight
+                else R.layout.schedule_widget_daynight
+                "night" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_night
+                else R.layout.schedule_widget_night
+                "day" -> if(isDynamicColorsEnabled) R.layout.schedule_widget_dynamic_day
+                else R.layout.schedule_widget_day
+                else -> R.layout.schedule_widget_daynight
+            }
+        }
     }
 }
--- composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/activities/WidgetSettingsActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ghostwalker18/schedule/activities/WidgetSettingsActivity.kt
@@ -14,21 +14,188 @@
 
 package com.ghostwalker18.schedule.activities
 
+import android.app.UiModeManager
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import com.ghostwalker18.schedule.utils.setContentWithTheme
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.ghostwalker18.schedule.R
+import com.ghostwalker18.schedule.ScheduleApp.Companion.getInstance
+import com.ghostwalker18.schedule.ScheduleWidget
+import com.ghostwalker18.schedule.models.ScheduleRepository
+import io.appmetrica.analytics.AppMetrica
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
 /**
  * Этот класс представляет собой экран настроек виджета приложения
  *
  * @author  Ипатов Никита
  * @since 2.3
+ * @see ScheduleWidget
  */
-class WidgetSettingsActivity : ComponentActivity() {
+class WidgetSettingsActivity
+    : AppCompatActivity(), View.OnClickListener, OnSharedPreferenceChangeListener {
+    private var fragment: SettingsFragment? = null
+    private var preferences: SharedPreferences? = null
+    private var preview: ImageView? = null
+    private var widgetID = AppWidgetManager.INVALID_APPWIDGET_ID
+    private var resultValue: Intent? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentWithTheme {
+        setContentView(R.layout.activity_widget_settings)
+        val addButton = findViewById<Button>(R.id.add)
+        addButton.setOnClickListener(this)
 
+        val intent = intent
+        val extras = intent.extras
+        if (extras != null) {
+            widgetID = extras.getInt(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID
+            )
+        }
+        if (widgetID == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+        }
+
+        fragment = SettingsFragment(widgetID)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, fragment!!)
+                .commit()
+        }
+        preferences = getSharedPreferences("WIDGET_$widgetID", MODE_PRIVATE)
+        preferences?.registerOnSharedPreferenceChangeListener(this)
+
+        preview = findViewById(R.id.widget_preview)
+
+        resultValue = Intent()
+        resultValue!!.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID)
+        setResult(RESULT_CANCELED, resultValue)
+    }
+
+    override fun onClick(view: View) {
+        setResult(RESULT_OK, resultValue)
+
+        if (getInstance().isAppMetricaActivated) AppMetrica.reportEvent("Добавили виджет")
+
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        preferences!!.edit()
+            .putBoolean("isEdited", true)
+            .commit()
+        ScheduleWidget.updateAppWidget(this, appWidgetManager, widgetID)
+        finish()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, s: String?) {
+        var previewName = "widget"
+
+        val isDynamicColors = preferences!!.getBoolean("dynamic_colors", false)
+        if (isDynamicColors) previewName += "_dynamic"
+
+        val theme = preferences!!.getString("theme", "system")!!
+        when (theme) {
+            "night" -> previewName += "_dark"
+            "day" -> previewName += "_light"
+            "system" -> {
+                val uiModeManager = getSystemService(UI_MODE_SERVICE) as UiModeManager
+                val currentNightMode = uiModeManager.nightMode
+                previewName += when (currentNightMode) {
+                    UiModeManager.MODE_NIGHT_YES -> "_dark"
+                    else -> "_light"
+                }
+            }
+        }
+
+        val day = preferences!!.getString("day", "today")!!
+        when (day) {
+            "today" -> previewName += "_today"
+            "tomorrow" -> previewName += "_tomorrow"
+        }
+
+        val imageId = resources.getIdentifier(previewName, "drawable", packageName)
+        preview!!.setImageResource(imageId)
+    }
+
+    class SettingsFragment
+        : PreferenceFragmentCompat, OnSharedPreferenceChangeListener {
+        private val repository: ScheduleRepository = getInstance().scheduleRepository
+        private var widgetId: Int = 0
+        private var preferences: SharedPreferences? = null
+        private var groupChoicePreference: ListPreference? = null
+        private val scope = CoroutineScope(Dispatchers.Main)
+
+        constructor(widgetId: Int) : super() {
+            this.widgetId = widgetId
+        }
+
+        constructor() : super()
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            if (savedInstanceState != null) widgetId = savedInstanceState.getInt("id")
+        }
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            outState.putInt("id", widgetId)
+            super.onSaveInstanceState(outState)
+        }
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            repository.update()
+
+            setPreferencesFromResource(R.xml.widget_preferences, rootKey)
+            preferenceManager.sharedPreferencesName = "WIDGET_$widgetId"
+            preferences = preferenceManager.sharedPreferences
+            preferences!!.registerOnSharedPreferenceChangeListener(this)
+
+            val dayChoicePreference = preferenceScreen.findPreference<ListPreference>("day")
+            dayChoicePreference!!.value = "today"
+            dayChoicePreference.summary = dayChoicePreference.entry
+
+            val themeChoicePreference = preferenceScreen.findPreference<ListPreference>("theme")
+            themeChoicePreference!!.value = "system"
+            themeChoicePreference.summary = themeChoicePreference.entry
+
+            groupChoicePreference = preferenceScreen.findPreference("group")
+            /**
+             * TODO: fix not attach to context bug
+             */
+            scope.launch {
+                repository.groups.collect { groups ->
+                    val groupsNew = groups.toMutableList()
+                    groupsNew.sortWith(Comparator.naturalOrder())
+
+                    groupsNew.add(0, getString(R.string.last_chosen))
+                    groupChoicePreference!!.entries = groupsNew.toTypedArray()
+
+                    groupsNew.removeAt(0)
+
+                    groupsNew.add(0, "last")
+                    groupChoicePreference!!.entryValues = groupsNew.toTypedArray()
+                    groupChoicePreference!!.summary = groupChoicePreference!!.entry
+                }
+            }
+        }
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, s: String?) {
+            val preference = preferenceScreen.findPreference<Preference>(s!!)
+            if (preference is ListPreference) {
+                preference.setSummary(preference.entry)
+            }
         }
     }
 }
